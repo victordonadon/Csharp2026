4eb3c1611b3afa847af25fc72360928de02456db
victordonadon <victorhdonadon@gmail.com>
2026-02-13 20:44:42 -0300

    aula03

---
+++ Lógica de programação e algoritmos/Aula 03/NaoSeiNada.cs
@@ -0,0 +1,7 @@
+double x , y , media;
+Console.WriteLine("Digite o primeiro numero: ");
+x = double.Parse(Console.ReadLine());
+Console.WriteLine("Digite o segundo numero: ");
+y = double.Parse(Console.ReadLine());
+media = (x+y)/2.0;
+Console.WriteLine("Media = " + media);
